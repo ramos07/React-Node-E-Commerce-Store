6e1663b5f0fba32666b98fe583ee5ef5a8911a51
ramos07 <ramosricardo96@gmail.com>
2020-10-05 11:51:28 -0700

    Added colors package to for easier visualization/identification of console log messages

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,9 +10,9 @@ const connectDB = async () => {
             useNewUrlParser: true,
             useCreateIndex: true,
         })
-        console.log(`MongoDB connected: ${conn.connection.host}`)
+        console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline)
     } catch (error) {
-        console.error(`Error: ${error.message}`)
+        console.error(`Error: ${error.message}`.red.underline.bold)
         process.exit(1)
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import colors from 'colors'
 import products from './data/products.js'
 
 // Initialize config file
@@ -28,6 +29,7 @@ const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
     console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green
+            .bold
     )
 })
