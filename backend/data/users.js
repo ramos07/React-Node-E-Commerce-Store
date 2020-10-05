fa8b4ea8cb3949857ce3c5674f5e8b5b16e2eb7f
ramos07 <ramosricardo96@gmail.com>
2020-10-05 12:16:43 -0700

    Installed bycryptjs to hash and encrypt passwords, created users array (seeding script), and removed id values from product.js objects

---
+++ backend/data/users.js
@@ -0,0 +1,18 @@
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: xxxx,
+        isAdmin: true,
+    },
+    {
+        name: 'John Wayne',
+        email: 'jwayne@example.com',
+        password: xxxx,
+    },
+    {
+        name: 'James Harden',
+        email: 'jharden@example.com',
+        password: xxxx,
+    },
+]
