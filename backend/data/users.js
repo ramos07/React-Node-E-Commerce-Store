1fc9d988565214acca5968590a6b33f5543e1ea0
ramos07 <ramosricardo96@gmail.com>
2020-10-05 12:36:36 -0700

    Hashed and encrypted test passwords with bcrypt

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,18 +1,25 @@
+import bcrypt from 'bcryptjs'
+
+/**
+ * Data to be used for seeder script
+ */
 const users = [
     {
         name: 'Admin User',
         email: 'admin@example.com',
-        password: xxxx,
+        password: bcrypt.hashSync('123456', 10), // Generate hash for password
         isAdmin: true,
     },
     {
         name: 'John Wayne',
         email: 'jwayne@example.com',
-        password: xxxx,
+        password: bcrypt.hashSync('123456', 10),
     },
     {
         name: 'James Harden',
         email: 'jharden@example.com',
-        password: xxxx,
+        password: bcrypt.hashSync('123456', 10),
     },
 ]
+
+export default users
