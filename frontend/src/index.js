eef67c78a60219ab7087dd92f8b6672216d79be3
ramos07 <ramosricardo96@gmail.com>
2020-10-05 14:19:19 -0700

    Installed redux and necessary deps. Also configured and set up Redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 import './bootstrap.min.css'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 )
 
 // If you want your app to work offline and load faster, you can change
