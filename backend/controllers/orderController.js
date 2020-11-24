26d5f8412aaa39b412a0bb825aeaf6cfd9993cfe
ramos07 <ramosricardo96@gmail.com>
2020-11-24 10:20:03 -0800

    Added getOrder route and controller to backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -38,4 +38,22 @@ const addOrderItems = asyncHandler(async (req, res) => {
     }
 })
 
-export { addOrderItems }
+// @desc Get an order by the id
+// @route GET /api/orders/:id
+// @access Private
+const getOrderById = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate(
+        'user',
+        'name email'
+    )
+
+    // if order exists else throw error
+    if (order) {
+        res.json(order)
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
+
+export { addOrderItems, getOrderById }
