3df2702cb0f7c27a93babe6d80142e0bbc7bf289
ramos07 <ramosricardo96@gmail.com>
2020-11-24 14:44:45 -0800

    Remove cartItems, shippingAddress, and paymentMethod after user logs out

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -58,6 +58,9 @@ export const login = (email, password) => async (dispatch) => {
 
 export const logout = () => (dispatch) => {
     localStorage.removeItem('userInfo')
+    localStorage.removeItem('cartItems')
+    localStorage.removeItem('shippingAddress')
+    localStorage.removeItem('paymentMethod')
     dispatch({
         type: USER_LOGOUT,
     })
